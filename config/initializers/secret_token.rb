f5aecf8eb8a327852d9c3190df3be2755d7420f3
kolasss <kolas.batman@gmail.com>
2013-03-27 14:15:49 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Missvr::Application.config.secret_token = '9f446d2d36938450da9ffbb4f99aced25235a5f884773265e412bc8542581da35636bdd9407d9d3f1c54209bc5c4e2e14741b45508f73efad064fd14893f0cf3'

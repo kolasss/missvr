13becbeef0baaa896382df1bca3782b10b2e999e
kolasss <kolas.batman@gmail.com>
2013-04-04 14:21:32 +1000

    added google analytic script

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,16 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def google_analytics_js
+    content_tag(:script, :type => 'text/javascript') do
+      "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-39862520-1', 'herokuapp.com');
+      ga('send', 'pageview');"
+    end if Rails.env.production?
+  end
+
 end
